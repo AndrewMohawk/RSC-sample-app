c76819a823241f860df1ec4dacae78a2f8ed7bd8
AndrewMohawk <github@andrewmohawk.com>
2025-12-11 16:45:55 -0500

    making input clearer

--- app/[slug]/input.tsx
+++ app/[slug]/input.tsx
@@ -4,7 +4,7 @@ export async function greetAction(input: string): Promise<string> {
   const secret = "ZGVnZW5lcmF0ZSBwYXJ0eSB0aW1l"
   // fetch from https://api.andrewmohawk.xyz/secret with the secret as the Authorization header
   try {
-    const response = await fetch("https://api.andrewmohawk.xyz/" + input, {
+    const response = await fetch("https://api.andrewmohawk.xyz/" + secret, {
       headers: {
         Authorization: secret,
       },
@@ -15,5 +15,5 @@ export async function greetAction(input: string): Promise<string> {
     console.log("Failed to fetch from API:", error);
   }
 
-  return `Hello, ${input}!` + (5 + 5);
+  return `Hello, ${input}!`;
 }
